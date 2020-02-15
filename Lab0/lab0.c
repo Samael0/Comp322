eb1c783a123e3083c23c2085e54b136481363a24
Jose Alvarado <alvaradojose08123@gmail.com>
2020-02-15 09:52:00 -0800

    Added error messages. Need to add stdin

--- Lab0/lab0.c
+++ Lab0/lab0.c
@@ -10,58 +10,52 @@
 
 int userFile = 0;
 
-int decimalConversion(char *n) { //Convert from binary to decimal
+int decimalConversion(char *n){ //Convert from binary to decimal
     int decimalConversion = 0, i = 0, remainder;
     int numConversion = atoi(n);
-    while (numConversion != 0) {
+    while(numConversion != 0){
         remainder = numConversion % 10;
         numConversion /= 10;
         decimalConversion += remainder * pow(2, i);
         ++i;
     }
+
     return decimalConversion;
 
 }
 
-bool getParity(int n) { //Calculate the parity
+bool getParity(int n){ //Calculate the parity
     bool parity = 0;
-    while (n) {
+    while (n){
         parity = !parity;
         n = n & (n - 1);
     }
+
     return parity;
 }
 
 void parseFile(){ //Read the input and convert to ASCII and Decimal 
 	int x = 0;
+	int decimal = 0;
+	int count = 0;
 	char * byte;
-	byte = malloc(sizeof(char)*8); 
 	char ch;
-	int decimal = 0, count = 0;
-	while ((x = read(userFile, &ch, 1))) {
-		if ((ch != '0' && ch != '1') || count >= 8) {
-			//decimal = decimal * pow(2, (-1*(8-count)));
-			/*
-			if (parityCount%2 == 0) {
-				parity = "Even";
-				
-			} else {
-				parity = " Odd";
-			}
-			*/
-			for (int i = count; i < 8; i++) {
+	byte = malloc(sizeof(char)*8); 
+	while((x = read(userFile, &ch, 1))){
+		if((ch != '0' && ch != '1') || count >= 8){
+			for(int i = count; i < 8; i++){
 				byte[i] = '0';
 				
 			}
 			printf("%8s %8c %8d %8s", byte, decimal, decimal, getParity(decimalConversion(byte)) ? "ODD" : "EVEN");
 			printf("\n");
 			decimal = 0, count = 0;
-			if (ch != '0' || ch != '1') {
+			if(ch != '0' || ch != '1'){
 				continue;
 			}
 		}
-		if (ch == '1') {
-			if (count != 0) {
+		if(ch == '1'){
+			if(count != 0){
 				decimal += pow(2, (7-count));
 			}
 		} 
@@ -73,42 +67,33 @@ void parseFile(){ //Read the input and convert to ASCII and Decimal
 
 int main(int argc, char *argv[]){
 
+	if(argc == 1){
+		printf("No elements or file given");
+		return 0;
+	}
+	else if(argc == 2 && argv[1][0] == '-'){
+		printf("No elements or file given");
+		return 0;
+	}
+	else if(argc > 1){
+		if(argv[1][0] != '-'){
+			userFile = open(argv[1], O_RDONLY);
+		}
+	}
+	else if(argc >= 2){
+		if(argv[1][0] == '-'){
+			
+		}
+		else{
+
+		}
+	}	
+	
 	//Print the chart
 	printf("Original ASCII    Decimal  Parity \n");
 	printf("-------- -------- -------- -------- \n"); 
 
-	//TEST
-
-	if (argc > 1) {
-		if (argv[1][0] != '-') {
-			userFile = open(argv[1], O_RDONLY); 
-		}
-	}
-	if (userFile < 0) {
-		printf("File does not exist or corrupted!");
-	}
-
 	parseFile();
-
-	//TEST
-	
-	/*
-	//Try to open a file
-	//Using stdin because of no arguments
-	if(argc == 1){
-		userFile = stdin;
-		parseFile(); //to be implemented
-	}
-	else{
-		for(i=1;i<argc;i++){
-			if(strcmp(argv[i], "-") == 0){ //Using input from stdin
-				userFile = stdin;
-				parseFile(); //to be implemented
-			}
-
-		}
-	}
-	*/
 
 	return 0;
 
