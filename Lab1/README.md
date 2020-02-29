fa749ff7900941209fff000705dd324e5abb818f
Jose Alvarado <alvaradojose08123@gmail.com>
2020-02-29 09:25:30 -0800

    Added function to grab and report info

--- Lab1/README.md
+++ Lab1/README.md
@@ -1,6 +1,10 @@
 # Lab 1 - Time and Forking Children
 
 ## To-Do
+* Makefile
+
+## Implemented
+* Base for the program
 * Print child info
 * Use these tools
     * time()
@@ -10,9 +14,8 @@
     * getpid()
     * exit()
 
-## Implemented
-* Base for the program
-
 ## Updates
 
 * Update 1: Added variables needed along with libraries. Next will be adding the rest of the function to have working program.
+
+* Update 2: Added a function that gets the info and prints it out. Implemented using the required functions. Need to add the times and print for the last step.
--- Lab1/time-4-baby-and-me.c
+++ Lab1/time-4-baby-and-me.c
@@ -4,11 +4,25 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+//Function in order to grab info
+void getInfo(pid_t tempPID, int ret){
+	waitpid(passP, &ret, 0); //option 0 used to terminate all PID's
+	printf("PID: %d PPID: %d", getpid(), getppid());
+	printf("CPID: %d RETVAL: %d", tempPID, ret);
+}
+
+//Main function
 int main(){
-	struct tms processTimes;
+	int status;
+	struct tms processTimes; //Structure containing the proccess times
 	time_t time = time(NULL);
 	pid_t waitT;
-	pid_t pid = fork();
+	pid_t pid = fork(); //Creates new process
 
 	printf("START: %ld\n", time);
+
+	if(pid < 0){ //Handle error in fork process
+		printf("Fork error\n");
+		exit(0);
+	}
 }
